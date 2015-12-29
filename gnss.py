93cf0621083055a4695acb5acc261667601f8f75
ChungChe <r94943159@ntu.edu.tw>
2015-12-29 17:23:31 +0800

    modified:   gnss.py write to utf-8

--- gnss.py
+++ gnss.py
@@ -1,4 +1,4 @@
-#!/usr/bin/python
+﻿#!/usr/bin/python
 # -*- coding: utf-8 -*-
 import sys
 import os
@@ -6,22 +6,22 @@ import codecs
 
 def gngsv(input_filename):
     f = codecs.open(input_filename, "r", "utf-8")
-    name = os.path.splitext(input_filename)[0]
-    fw = codecs.open(name+"GN.txt", "w")
-    fgp = codecs.open(name+"GP.csv", "w", "utf-8")
-    fgp.write("星系,數目,編號,總數,PRN,高度,方位角,CN\n" .encode('utf-8')
-    fgl = codecs.open(name+"GL.csv", "w", "utf-8")
-    fgl.write("星系,數目,編號,總數,PRN,高度,方位角,CN\n" .encode('utf-8')
-    fbd = codecs.ope(name+"BD.csv", "w", "utf-8")
-    fbd.write("星系,數目,編號,總數,PRN,高度,方位角,CN\n".encode('utf-8')
     if f == None:
         return
+    name = os.path.splitext(input_filename)[0]
+    fw = codecs.open(name+"GN.txt", "w", "utf-8")
+    fgp = codecs.open(name+"GP.csv", "w", "utf-8")
+    fgp.write(u"星系,數目,編號,總數,PRN,高度,方位角,CN\n")
+    fgl = codecs.open(name+"GL.csv", "w", "utf-8")
+    fgl.write(u"星系,數目,編號,總數,PRN,高度,方位角,CN\n")
+    fbd = codecs.open(name+"BD.csv", "w", "utf-8")
+    fbd.write(u"星系,數目,編號,總數,PRN,高度,方位角,CN\n")
     lines = f.readlines()
     for line in lines:
         if "GPGSV" in line:
             #print line,
             fw.write(line)
-            fgp.write(line)
+            fgp.write(line),
         if "GLGSV" in line:
             #print line,
             fw.write(line)
